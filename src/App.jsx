426f61211440ba50d2fac8cc2caa9e976f84a20c
Jérôme Abel <contact@jeromeabel.net>
2023-02-21 11:27:16 +0100

    RTK Query - first commit mocked pokemons data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,70 @@
-import Counter from './features/counter/Counter';
-import Products from './features/products/Products';
-import Cart from './features/cart/Cart';
+import { useState } from 'react';
+import POKEMONS from './data';
+import { POKEMONS_DETAILS } from './data';
+
 function App() {
+  const [selectedPokemon, selectPokemon] = useState(undefined);
+
   return (
     <>
-      <Products />
-      <Cart />
-      <Counter />
+      <header>
+        <h1>My Pokedex</h1>
+      </header>
+      <main>
+        {selectedPokemon ? (
+          <>
+            <PokemonDetails pokemonName={selectedPokemon} />
+            <button onClick={() => selectPokemon(undefined)}>back</button>
+          </>
+        ) : (
+          <PokemonList onPokemonSelected={selectPokemon} />
+        )}
+      </main>
     </>
   );
 }
 
 export default App;
+
+function PokemonList({ onPokemonSelected }) {
+  const data = POKEMONS;
+
+  return (
+    <article>
+      <h2>Overview</h2>
+      <ol start={1}>
+        {data.results.map((pokemon) => (
+          <li key={pokemon.name}>
+            <button onClick={() => onPokemonSelected(pokemon.name)}>
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
+      </ol>
+    </article>
+  );
+}
+
+const listFormatter = new Intl.ListFormat('en-GB', {
+  style: 'short',
+  type: 'conjunction',
+});
+function PokemonDetails({ pokemonName }) {
+  const data = POKEMONS_DETAILS;
+
+  return (
+    <article>
+      <h2>{data.name}</h2>
+      <img src={data.sprites.front_default} alt={data.name} />
+      <ul>
+        <li>id: {data.id}</li>
+        <li>height: {data.height}</li>
+        <li>weight: {data.weight}</li>
+        <li>
+          types:
+          {listFormatter.format(data.types.map((item) => item.type.name))}
+        </li>
+      </ul>
+    </article>
+  );
+}
